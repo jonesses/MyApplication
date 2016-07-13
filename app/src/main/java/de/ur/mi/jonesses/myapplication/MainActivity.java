2a968baba02129f38ef078098d574b32157e16e9
Jonas <jonesses@gmail.com>
2016-07-13 12:46:05 +0200

    second

--- app/src/main/java/de/ur/mi/jonesses/myapplication/MainActivity.java
+++ app/src/main/java/de/ur/mi/jonesses/myapplication/MainActivity.java
@@ -15,8 +15,7 @@ import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
-
-   //Comment
+    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
